4d8a5da8983e897b88ac63f5961c8070cfded2f3
Sivann <98021790+CodeWAds@users.noreply.github.com>
2023-10-10 11:48:20 +0300

    Add files via upload
    Отчёт по ОАиП. Домашняя контрольная работа №1

---
+++ Otchet_1/kontrrab_1.pas
@@ -0,0 +1,20 @@
+﻿program function1;
+var
+  x, res: real;
+begin
+  x := -11;
+  while x <= 6 do
+  begin
+    if x < -9 then
+      res := cos(x) + ln(-x)
+    else if (x >= -9) and (x < 0) then
+      res := sin(x) - cos(x)
+    else if (x >= 0) and (x < 4) then
+      res := log10(x) / ln(x) - (power(x, 1/3) / 55)
+    else
+      res := ln(x);
+
+    writeln('Для x = ', x, ' Результат: ', res);
+    x := x + 0.3;
+  end;
+end.
