62330718ded79df7ffc8ac20141052e33f640929
Sivann <sitnikovivan10@mail.ru>
2023-11-15 18:13:27 +0300

    add otchet2

---
+++ Otchet_2/zadacha1_kontr_rab_2.pas
@@ -0,0 +1,30 @@
+﻿program zadacha1;
+
+const
+  Size = 10;
+
+var
+  arr: array[1..Size] of integer;
+  n, i, j, count: integer;
+
+begin
+  writeln('Введите элементы массива: ');
+  for i := 1 to Size do
+    readln(arr[i]);
+
+  count := 0;
+
+  for i := 1 to Size do
+  begin
+    for j := 1 to i do
+    begin
+      if arr[i] = arr[j] then
+        break; 
+    end;
+
+    if j = i then
+      Inc(count);
+  end;
+
+  writeln('Количество различных элементов в массиве: ', count);
+end.
---
+++ Otchet_2/zadacha2_kontr_rab_2.pas
@@ -0,0 +1,31 @@
+﻿program zadacha2;
+
+var
+  str: string;
+  i, len: integer;
+  isPal: boolean;
+
+begin
+  writeln('Введите строку: ');
+  readln(str);
+
+  str := LowerCase(str);
+
+  len := Length(str);
+
+  isPal := True;
+  for i := 1 to len div 2 do
+  begin
+    if str[i] <> str[len - i + 1] then
+    begin
+      isPal := False;
+      Break;
+    end;
+  end;
+
+  if isPal then
+    writeln('Строка - палиндром')
+  else
+    writeln('Строка - не палиндром');
+
+end.
